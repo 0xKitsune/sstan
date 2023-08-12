b77898c406511be5444135a8cadd92bbf98627aa
LeytonTaylor <leytontaylor23@gmail.com>
2023-08-12 13:06:44 -0700

    #49

--- src/analyzer/optimizations/string_errors.rs
+++ src/analyzer/optimizations/string_errors.rs
@@ -4,35 +4,33 @@ use solang_parser::pt::{self, Loc};
 use solang_parser::{self, pt::SourceUnit};
 
 use crate::analyzer::ast::{self, Target};
-use crate::analyzer::utils;
+use crate::analyzer::extractors::primitive::FunctionCallExtractor;
+use crate::analyzer::extractors::{compound::SolidityVerisonExtractor, Extractor};
 
-pub fn string_error_optimization(source_unit: SourceUnit) -> HashSet<Loc> {
+pub fn string_error_optimization(source_unit: &mut SourceUnit) -> eyre::Result<HashSet<Loc>> {
     //Create a new hashset that stores the location of each optimization target identified
     let mut optimization_locations: HashSet<Loc> = HashSet::new();
 
-    let solidity_version = utils::get_solidity_version_from_source_unit(source_unit.clone())
-        .expect("Could not extract Solidity version from source unit.");
+    let solidity_versions = SolidityVerisonExtractor::extract(source_unit)?;
+    for version in solidity_versions.into_iter().flatten() {
+        if version.minor >= 8 && version.patch >= 4 {
+            //Extract the target nodes from the source_unit
+            let target_nodes = FunctionCallExtractor::extract(source_unit)?;
 
-    if solidity_version.1 >= 8 && solidity_version.2 >= 4 {
-        //Extract the target nodes from the source_unit
-        let target_nodes = ast::extract_target_from_node(Target::FunctionCall, source_unit.into());
-
-        for node in target_nodes {
-            //We can use unwrap because Target::FunctionCall is an expression
-            let expression = node.expression().unwrap();
-
-            if let pt::Expression::FunctionCall(_, function_identifier, func_call_expressions) =
-                expression
-            {
-                //if the function call identifier is a variable
-                if let pt::Expression::Variable(identifier) = *function_identifier {
-                    //if the identifier name is "require"
-                    if identifier.name == *"require" {
-                        //If the require statement contains strings
-                        if let Some(pt::Expression::StringLiteral(vec_string_literal)) =
-                            func_call_expressions.last()
-                        {
-                            optimization_locations.insert(vec_string_literal[0].loc);
+            for node in target_nodes {
+                if let pt::Expression::FunctionCall(_, function_identifier, func_call_expressions) =
+                    node
+                {
+                    //if the function call identifier is a variable
+                    if let pt::Expression::Variable(identifier) = *function_identifier {
+                        //if the identifier name is "require"
+                        if identifier.name == *"require" {
+                            //If the require statement contains strings
+                            if let Some(pt::Expression::StringLiteral(vec_string_literal)) =
+                                func_call_expressions.last()
+                            {
+                                optimization_locations.insert(vec_string_literal[0].loc);
+                            }
                         }
                     }
                 }
@@ -40,7 +38,7 @@ pub fn string_error_optimization(source_unit: SourceUnit) -> HashSet<Loc> {
         }
     }
     //Return the identified optimization locations
-    optimization_locations
+    Ok(optimization_locations)
 }
 #[test]
 fn test_string_error_optimization() {
@@ -61,11 +59,11 @@ fn test_string_error_optimization() {
      }
      "#;
 
-    let source_unit = solang_parser::parse(file_contents, 0).unwrap().0;
+    let mut source_unit = solang_parser::parse(file_contents, 0).unwrap().0;
 
-    let optimization_locations = string_error_optimization(source_unit);
+    let optimization_locations = string_error_optimization(&mut source_unit);
 
-    assert_eq!(optimization_locations.len(), 3);
+    assert_eq!(optimization_locations.unwrap().len(), 3);
 
     //test when base solidiy version is < than 0.8.4
     let file_contents_1 = r#"
@@ -84,9 +82,9 @@ fn test_string_error_optimization() {
      }
      "#;
 
-    let source_unit_1 = solang_parser::parse(file_contents_1, 0).unwrap().0;
+    let mut source_unit_1 = solang_parser::parse(file_contents_1, 0).unwrap().0;
 
-    let optimization_locations_1 = string_error_optimization(source_unit_1);
+    let optimization_locations_1 = string_error_optimization(&mut source_unit_1);
 
-    assert_eq!(optimization_locations_1.len(), 0);
+    assert_eq!(optimization_locations_1.unwrap().len(), 0);
 }
