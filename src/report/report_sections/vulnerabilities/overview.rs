1e86352e22e1b74d6de790e1cddefaf04a3b644f
0xKitsune <0xKitsune@protonmail.com>
2023-08-21 02:37:45 -0400

    updated vuln heading

--- src/report/report_sections/vulnerabilities/overview.rs
+++ src/report/report_sections/vulnerabilities/overview.rs
@@ -1,5 +1,5 @@
 pub fn report_section_content(total_vulnerabilities: usize) -> String {
-    format!("# Gas Optimizations - (Total Vulnerabilities {})\n
+    format!("# Vulnerabilities - (Total Vulnerabilities {})\n
 The following sections detail the high, medium and low severity vulnerabilities found throughout the codebase.\n\n<br>\n
 ", total_vulnerabilities)
 }
