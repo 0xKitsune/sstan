b0ddd2a71f58ac79675a3fc8e93572b89ca36708
LeytonTaylor <leytontaylor23@gmail.com>
2023-09-01 21:14:17 -0700

    constant variable

---
+++ src/optimizations/constant_variable.rs
@@ -0,0 +1,211 @@
+use std::collections::{HashMap, HashSet};
+use std::path::PathBuf;
+
+use solang_parser::pt::{self, CodeLocation, Loc};
+use solang_parser::{self, pt::SourceUnit};
+
+use super::{CacheStorageInMemory, ConstantVariable, OptimizationOutcome, OptimizationPattern};
+use crate::engine::{EngineError, Outcome, Pushable};
+use crate::extractors::primitive::IncrementorExtractor;
+use crate::extractors::{primitive::AssignmentExtractor, Extractor};
+use crate::utils::get_32_byte_storage_variables;
+
+impl OptimizationPattern for ConstantVariable {
+    fn find(source: &mut HashMap<PathBuf, SourceUnit>) -> Result<OptimizationOutcome, EngineError> {
+        let mut outcome = Outcome::new();
+        for (path_buf, source_unit) in source {
+            let mut optimization_locations: HashSet<Loc> = HashSet::new();
+
+            let mut storage_variables = get_32_byte_storage_variables(source_unit, true, false);
+
+            let assignment_nodes = AssignmentExtractor::extract(source_unit)?;
+            let incrementor_nodes = IncrementorExtractor::extract(source_unit)?;
+            let nodes = assignment_nodes
+                .into_iter()
+                .chain(incrementor_nodes.into_iter())
+                .collect::<Vec<pt::Expression>>();
+
+            for node in nodes {
+                match node {
+                    pt::Expression::Assign(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::PreIncrement(_, box_expression) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::PostIncrement(_, box_expression) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::PreDecrement(_, box_expression) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::PostDecrement(_, box_expression) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+
+                    pt::Expression::AssignAdd(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignAnd(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignDivide(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignModulo(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignMultiply(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignOr(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignShiftLeft(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignShiftRight(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignSubtract(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    pt::Expression::AssignXor(_, box_expression, _) => {
+                        if let pt::Expression::Variable(identifier) = *box_expression {
+                            //if the variable name exists in the storage variable hashmap
+                            if storage_variables.contains_key(&identifier.name) {
+                                //if the variable has been used, remove it from storage variables
+                                storage_variables.remove(&identifier.name);
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            }
+
+            //if the variable is not been reassigned, add it to the optimization locations
+            for variable in storage_variables {
+                outcome.push_or_insert(path_buf.clone(), variable.1.loc(), variable.1.to_string());
+            }
+        }
+
+        Ok(OptimizationOutcome::ConstantVariable(outcome))
+    }
+}
+mod test {
+    use super::OptimizationPattern;
+    use crate::{optimizations::ConstantVariable, utils::MockSource};
+    #[test]
+    fn test_constant_variable_optimization() -> eyre::Result<()> {
+        let file_contents = r#"
+    
+    pragma solidity >= 0.8.0;
+    contract Contract {
+
+
+        uint256 firstUint256 = 0;
+        uint256 secondUint256 = 100;
+        uint256 immutable thirdUint256 = 100;
+        uint256 fourthUint256 = 100;
+        uint256 constant fifthUint256 = 1000000;
+
+       
+        function testFunction() public {
+            firstUint256 = 10;
+            secondUint256 = someVal;
+        }
+    }
+ 
+    "#;
+
+        let mut source = MockSource::new().add_source("constant_variable.sol", file_contents);
+        let optimization_locations = ConstantVariable::find(&mut source.source)?;
+        assert_eq!(optimization_locations.len(), 2);
+
+        Ok(())
+    }
+}
--- src/report/mocks/qa_report_sections.md
+++ src/report/mocks/qa_report_sections.md
@@ -1,6 +1,17 @@
 
  <details open> 
  <summary> 
+ <Strong>Constructor should initialize all variables</Strong> Instances(1) 
+ </summary> 
+ Description of the qa pattern goes here 
+
+ <span style="color: green;">File: </span> constructor_var_initialization.sol 5-5 
+ ```solidity 
+ constructor(address _owner) {owner = _owner} 
+ ``` 
+ </details>
+ <details open> 
+ <summary> 
  <Strong>Constructor should be listed before any other function</Strong> Instances(1) 
  </summary> 
  Description of the qa pattern goes here 
@@ -10,6 +21,7 @@
  constructor() {owner = address(1)} 
  ``` 
  </details>
+
  <details open> 
  <summary> 
  <Strong>Interface names should start with an I</Strong> Instances(1) 
@@ -19,6 +31,65 @@
  <span style="color: green;">File: </span> interface_namespace.sol 4-0 
  ```solidity 
  interface Contract0 {} 
+ ``` 
+ </details>
+
+
+ <details open> 
+ <summary> 
+ <Strong>Consider using scientific notation for large multiples of 10</Strong> Instances(1) 
+ </summary> 
+ For example 100000 can be written as 1e5 
+
+ <span style="color: green;">File: </span> large_multiples_of_ten.sol 12-12 
+ ```solidity 
+ uint256 x = 10000000; 
+ ``` 
+ </details>
+
+ <details open> 
+ <summary> 
+ <Strong>Private variables should contain a leading underscore</Strong> Instances(3) 
+ </summary> 
+ Description of the qa pattern goes here 
+
+ <span style="color: green;">File: </span> private_vars.sol 5-5 
+ ```solidity 
+ address public _addr2; 
+ ```
+
+ <span style="color: green;">File: </span> private_vars.sol 7-7 
+ ```solidity 
+ address private addr4; 
+ ```
+
+ <span style="color: green;">File: </span> private_vars.sol 9-9 
+ ```solidity 
+ address internal addr6; 
+ ``` 
+ </details>
+ <details open> 
+ <summary> 
+ <Strong>Remove any unused returns</Strong> Instances(1) 
+ </summary> 
+  
+
+ <span style="color: green;">File: </span> unused_returns.sol 5-5 
+ ```solidity 
+ function foo() public returns (uint256 x) {uint256 y = 0; return y;} 
+ ``` 
+ </details>
+
+
+ <details open> 
+ <summary> 
+ <Strong>Remove any unused functions</Strong> Instances(1) 
+ </summary> 
+  
+
+ <span style="color: green;">File: </span> unused_functions.sol 4-4 
+ ```solidity 
+ function isUnused() internal {} 
  ``` 
  </details>
 
@@ -36,72 +107,18 @@
 
  <details open> 
  <summary> 
- <Strong>Constructor should initialize all variables</Strong> Instances(1) 
+ <Strong>Storage variables should be named with camel case</Strong> Instances(2) 
  </summary> 
- Description of the qa pattern goes here 
+ Consider renaming to follow convention 
 
- <span style="color: green;">File: </span> constructor_var_initialization.sol 5-5 
+ <span style="color: green;">File: </span> storage_var_namespace.sol 4-4 
  ```solidity 
- constructor(address _owner) {owner = _owner} 
- ``` 
- </details>
-
-
- <details open> 
- <summary> 
- <Strong>Private variables should contain a leading underscore</Strong> Instances(3) 
- </summary> 
- Description of the qa pattern goes here 
-
- <span style="color: green;">File: </span> private_vars.sol 9-9 
- ```solidity 
- address internal addr6; 
+ address IS_NOT_FINE; 
  ```
 
- <span style="color: green;">File: </span> private_vars.sol 5-5 
+ <span style="color: green;">File: </span> storage_var_namespace.sol 7-7 
  ```solidity 
- address public _addr2; 
- ```
-
- <span style="color: green;">File: </span> private_vars.sol 7-7 
- ```solidity 
- address private addr4; 
- ``` 
- </details>
-
- <details open> 
- <summary> 
- <Strong>Consider using scientific notation for large multiples of 10</Strong> Instances(1) 
- </summary> 
- For example 100000 can be written as 1e5 
-
- <span style="color: green;">File: </span> large_multiples_of_ten.sol 12-12 
- ```solidity 
- uint256 x = 10000000; 
- ``` 
- </details>
-
- <details open> 
- <summary> 
- <Strong>Remove any unused functions</Strong> Instances(1) 
- </summary> 
-  
-
- <span style="color: green;">File: </span> unused_functions.sol 4-4 
- ```solidity 
- function isUnused() internal {} 
- ``` 
- </details>
-
- <details open> 
- <summary> 
- <Strong>Remove any unused returns</Strong> Instances(1) 
- </summary> 
-  
-
- <span style="color: green;">File: </span> unused_returns.sol 5-5 
- ```solidity 
- function foo() public returns (uint256 x) {uint256 y = 0; return y;} 
+ address ALSO_IS_BAD; 
  ``` 
  </details>
 
@@ -119,22 +136,5 @@
  <span style="color: green;">File: </span> constant_immutable.sol 6-6 
  ```solidity 
  address immutable Is_Bad; 
- ``` 
- </details>
-
- <details open> 
- <summary> 
- <Strong>Storage variables should be named with camel case</Strong> Instances(2) 
- </summary> 
- Consider renaming to follow convention 
-
- <span style="color: green;">File: </span> storage_var_namespace.sol 4-4 
- ```solidity 
- address IS_NOT_FINE; 
- ```
-
- <span style="color: green;">File: </span> storage_var_namespace.sol 7-7 
- ```solidity 
- address ALSO_IS_BAD; 
  ``` 
  </details>
