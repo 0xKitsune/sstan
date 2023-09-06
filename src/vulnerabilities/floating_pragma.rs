4fedf63331acbb55e4e4a00580e03e502d87dcd1
0xKitsune <0xKitsune@protonmail.com>
2023-09-05 23:29:25 -0400

    updated vuln macros, migrated floating pragma

---
+++ src/vulnerabilities/floating_pragma.rs
@@ -0,0 +1,70 @@
+use std::collections::{HashMap, HashSet};
+use std::fs::File;
+use std::path::PathBuf;
+
+use solang_parser::pt::{self, Expression, Loc};
+use solang_parser::{self, pt::SourceUnit};
+
+use crate::engine::{EngineError, Outcome, Pushable};
+use crate::extractors::{
+    primitive::{AssignmentExtractor, UrnaryOpteratorExtractor},
+    Extractor,
+};
+use crate::report::ReportSectionFragment;
+use crate::utils::MockSource;
+use std::io::Write;
+
+use super::{DivideBeforeMultiply, FloatingPragma, VulnerabilityOutcome, VulnerabilityPattern};
+
+impl VulnerabilityPattern for FloatingPragma {
+    fn find(
+        source: &mut HashMap<PathBuf, SourceUnit>,
+    ) -> Result<VulnerabilityOutcome, EngineError> {
+        let mut vulnerability_locations = Outcome::new();
+
+        for (path_buf, source_unit) in source {
+            //Extract the target nodes from the source_unit
+            let target_nodes = PragmaDirectiveExtractor::extract(source_unit)?;
+
+            //For each target node that was extracted, check for the vulnerability patterns
+            for node in target_nodes {
+                //We can use unwrap because Target::PragmaDirective is a source unit part
+                if let pt::SourceUnitPart::PragmaDirective(loc, _, Some(pragma)) = node {
+                    if pragma.string.contains('^') {
+                        vulnerability_locations.insert(path_buf.clone(), (loc, node.to_string()));
+                    }
+                }
+            }
+        }
+
+        Ok(VulnerabilityOutcome::DivideBeforeMultiply(
+            vulnerability_locations,
+        ))
+    }
+}
+
+#[test]
+fn test_floating_pragma_vulnerability() -> eyre::Result<()> {
+    let file_contents = r#"
+
+    pragma solidity ^0.8.16;
+
+    contract Contract0 {
+
+    }
+    "#;
+
+    let mut mock_source = MockSource::new().add_source("floating_pragma.sol", file_contents);
+    let qa_locations = DivideBeforeMultiply::find(&mut mock_source.source)?;
+    assert_eq!(qa_locations.len(), 1);
+
+    let report: Option<ReportSectionFragment> = qa_locations.into();
+    if let Some(report) = report {
+        let mut f = File::options()
+            .append(true)
+            .open("src/report/mocks/qa_report_sections.md")?;
+        writeln!(&mut f, "{}", &String::from(report))?;
+    }
+
+    Ok(())
+}
