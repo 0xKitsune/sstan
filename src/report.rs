4214098fccd89088e62a11b8998769b1d0c9860e
LeytonTaylor <leytontaylor23@gmail.com>
2023-11-15 10:16:15 -0700

    serialize OutcomeReport

--- src/report.rs
+++ src/report.rs
@@ -370,6 +370,29 @@ pub struct OutcomeReport {
     pub file_path: PathBuf,
 }
 
+impl Serialize for OutcomeReport {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+            
+            let mut state = serializer.serialize_struct("OutcomeReport", 2)?;
+            let mut content = String::new();
+            if let Ok(lines) = read_lines(self.file_path.as_path()) {
+                for (i, line) in lines.enumerate() {
+                    if let Ok(l) = line {
+                        if i + 1 >= self.line_numbers.0 && i < self.line_numbers.1 {
+                            content.push_str(&format!("{}:{}\n", i, l));
+                        }
+                    }
+                }
+            }
+            state.serialize_field("content", content.as_str())?;
+            state.serialize_field("loc", self.file_path.as_os_str())?;//TODO: Add git url somewhere in here to derive permalink
+            state.end()
+    }
+}
+
 impl OutcomeReport {
     pub fn new(
         file_name: String,
